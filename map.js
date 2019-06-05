9c55f19060b084ae1a09e075f54625bf44e0613e
billtsai1518 <billtsai1518@gmail.com>
2019-06-05 17:38:33 +0800

    update geolocation

--- map.js
+++ map.js
@@ -1,5 +1,14 @@
-var ntust_map = L.map('ntustmap').setView([25.0134707,121.5419075], 18);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: '<a href="https://www.openstreetmap.org/">OSM</a>', maxZoom: 19,}).addTo(ntust_map);
+var ntust_map = L.map('ntustmap').setView([25.0134707,121.5419075], 18).locate({setView: true, watch: true});
+
+//標記所在位置
+function onLocationFound(e) {
+    var radius = e.accuracy / 2;
+    L.marker(e.latlng).addTo(ntust_map).openPopup();
+    L.circle(e.latlng, radius, {fillOpacity: 0.1}).addTo(ntust_map);
+}
+ntust_map.on('locationfound', onLocationFound);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: '<a href="https://www.openstreetmap.org/">OSM</a>', maxZoom: 19}).addTo(ntust_map);
 
 var ib_marker = L.marker([25.01304,121.54013]);
 ib_marker.addTo(ntust_map);
