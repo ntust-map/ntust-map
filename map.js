21edd772c2311163b9824c30afa9a2110c6967d5
billtsai1518 <billtsai1518@gmail.com>
2019-06-06 15:51:44 +0800

    Remove openPopup() and update comments

--- map.js
+++ map.js
@@ -1,3 +1,4 @@
+//Map init
 var ntust_map = L.map('ntustmap').setView([25.0134707,121.5419075], 18).locate({setView: true});
 
 //標記所在位置
@@ -8,9 +9,10 @@ function onLocationFound(e) {
 }
 ntust_map.on('locationfound', onLocationFound);
 
+//Display tile layers on the map with OpenStreetMap
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: '<a href="https://www.openstreetmap.org/">OSM</a>', maxZoom: 19}).addTo(ntust_map);
 
+//國際大樓的marker
 var ib_marker = L.marker([25.01304,121.54013]);
 ib_marker.addTo(ntust_map);
-
-ib_marker.bindPopup("<strong>國際大樓</strong>").openPopup();
+ib_marker.bindPopup("<strong>國際大樓</strong>");
