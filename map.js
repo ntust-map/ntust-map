8f7a78435e88ebe92f83f5321fa6191f525d475f
billtsai1518 <billtsai1518@gmail.com>
2019-06-08 17:14:27 +0800

    Overlay objects update

--- map.js
+++ map.js
@@ -1,18 +1,32 @@
-//Map init
-var ntust_map = L.map('ntustmap').setView([25.0134707,121.5419075], 18).locate({setView: true});
+//Layer group init
+var ib_group = L.layerGroup();
+var tr_group = L.layerGroup();
 
-//標記所在位置
+//Add building marker to layer group
+L.marker([25.01304,121.54013]).bindPopup("<strong>國際大樓</strong>").addTo(ib_group);
+L.marker([25.01505,121.54280]).bindPopup("<strong>研揚大樓</strong>").addTo(tr_group);
+
+//Overlay menu init, including layer group
+var overlayMarkers = {
+    "國際大樓(IB)": ib_group,
+    "研揚大樓(TR)": tr_group
+};
+
+//Map init
+var ntust_map = L.map('ntustmap', {layers: [ib_group, tr_group]}).setView([25.0134707,121.5419075], 18).locate({setView: true});
+
+//Display tile layers on the map with OpenStreetMap
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: '<a href="https://www.openstreetmap.org/">OSM</a>', maxZoom: 19}).addTo(ntust_map);
+
+//Add overlay menu to map
+//layers(base layers object, overlays object)
+//          radioboxes          checkboxes 
+L.control.layers(null, overlayMarkers).addTo(ntust_map);
+
+//GPS location
 function onLocationFound(e) {
     var radius = e.accuracy / 2;
     L.marker(e.latlng).addTo(ntust_map).openPopup();
     L.circle(e.latlng, radius, {fillOpacity: 0.1}).addTo(ntust_map);
 }
 ntust_map.on('locationfound', onLocationFound);
-
-//Display tile layers on the map with OpenStreetMap
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: '<a href="https://www.openstreetmap.org/">OSM</a>', maxZoom: 19}).addTo(ntust_map);
-
-//國際大樓的marker
-var ib_marker = L.marker([25.01304,121.54013]);
-ib_marker.addTo(ntust_map);
-ib_marker.bindPopup("<strong>國際大樓</strong>");
