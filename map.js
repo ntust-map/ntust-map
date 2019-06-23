84729fe646b3a489f147be3335a442720aa26925
billtsai1518 <billtsai1518@gmail.com>
2019-06-23 21:16:43 +0800

    Update layer control, and add seminar and club group

--- map.js
+++ map.js
@@ -1,19 +1,32 @@
 //Layer group init
+var all_activity_group = L.layerGroup();
+var seminar_group = L.layerGroup();
+var club_group = L.layerGroup();
+
 var ib_group = L.layerGroup();
 var tr_group = L.layerGroup();
 
 //Add building marker to layer group
-L.marker([25.01304,121.54013]).bindPopup("<strong>國際大樓</strong>").addTo(ib_group);
-L.marker([25.01505,121.54280]).bindPopup("<strong>研揚大樓</strong>").addTo(tr_group);
+L.marker([25.01304,121.54013]).bindPopup("<strong>模擬聯合國社課</strong><br>地點：IB-303").addTo(ib_group).addTo(club_group).addTo(all_activity_group);
+L.marker([25.01278,121.54030]).bindPopup("<strong>宇宙學研討會</strong><br>地點：IB-102").addTo(ib_group).addTo(seminar_group).addTo(all_activity_group);
+L.marker([25.01326,121.54031]).bindPopup("<strong>期末設計展</strong><br>地點：IB-101").addTo(ib_group).addTo(all_activity_group);
+
+L.marker([25.01505,121.54280]).bindPopup("<strong>研揚大樓</strong>").addTo(tr_group).addTo(all_activity_group);
+
+var activityMarkers = {
+    "所有活動": all_activity_group,
+    "研討會": seminar_group,
+    "社團": club_group,
+};
 
 //Overlay menu init, including layer group
-var overlayMarkers = {
+var placeMarkers = {
     "國際大樓(IB)": ib_group,
-    "研揚大樓(TR)": tr_group
+    "研揚大樓(TR)": tr_group,
 };
 
 //Map init
-var ntust_map = L.map('ntustmap', {layers: [ib_group, tr_group]}).setView([25.0134707,121.5419075], 18).locate({setView: true});
+var ntust_map = L.map('ntustmap', {layers: [all_activity_group, ib_group, tr_group]}).setView([25.0134707,121.5419075], 18).locate({setView: true});
 
 //Display tile layers on the map with OpenStreetMap
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: '<a href="https://www.openstreetmap.org/">OSM</a>', maxZoom: 19}).addTo(ntust_map);
@@ -21,7 +34,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: 
 //Add overlay menu to map
 //layers(base layers object, overlays object)
 //          radioboxes          checkboxes 
-L.control.layers(null, overlayMarkers).addTo(ntust_map);
+L.control.layers(activityMarkers, placeMarkers).addTo(ntust_map);
 
 //GPS location
 function onLocationFound(e) {
